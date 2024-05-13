ed5db58f914114e113fbcb90232afacc97d67654
patrici06 <patriciorivero77@gmail.com>
2024-05-13 16:19:21 -0300

    documentacion

--- SGE/Repositorios/obj/Debug/net8.0/Repositorios.AssemblyInfo.cs
+++ SGE/Repositorios/obj/Debug/net8.0/Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a07463e25700636843633a024b184a91b9d9de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+180287a68bce55cf29b06fd49702c2ddc92ef9b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
